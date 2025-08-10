7c9ae1bd0d24c59bf829b7c2a346ebb0deb16fd2
Adrián Rincón <adrianrincon022@gmail.com>
2025-08-10 18:43:08 -0500

    Se crea función 'agregarAmigo' donde por medio del ID se guarda el valor ingresado en el input, esta función se añade al botón por medio de onclick.

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let amigos = []; 
+let amigos = [];
+let amigo ='';
+
+function agregarAmigo(){
+    amigo = document.getElementById("amigo").value;
+    if (amigo == ''){
+        alert ('Por favor ingresar un noombre valido.');
+    }else {
+        amigos.push(amigo);
+        document.getElementById("amigo").value = '';
+    }
+    document.getElementById('amigo').focus();
+}
