93bee68a519b27cf9044d228ffb4705727a8756b
Adrián Rincón <adrianrincon022@gmail.com>
2025-08-11 11:28:57 -0500

    Se crea función sortearAmigo la cual al darle clic en el boton sortear compara si esta vacia la lista, o solo un caracter, si esta tiene dos o más le permite jugar, tomando por medio del id el lu donde de manera aleatoria introduce unos de los nombre ingresados

--- app.js
+++ app.js
@@ -16,13 +16,30 @@ function agregarAmigo(){
 }
 
 function imprimirLista() {
+    // Obtenemos el elemento "ul" donde se mostraran los elemento "li"
     let lista = document.getElementById("listaAmigos");
+    // limpiamos la lista para que no se repitan
     lista.innerHTML='';
+
     for (let nombre of amigos) {
         let li = document.createElement("li");
         li.innerHTML = nombre;
 
         lista.appendChild(li);
+    }
+}
+
+function sortearAmigo(){
+    let amigoSecreto = document.getElementById("resultado");
+    let indice = Math.floor(Math.random() * amigos.length);
+    let random = amigos[indice];
+
+    if (amigos == ''){
+        alert("!No has introducido 'Amigos' para sortear¡");
+    }else if(amigos.length <= 1){
+        alert ("Debes introducir al menos dos personas para jugar");
+    }else {
+        amigoSecreto.innerHTML = random;
     }
 }
 
